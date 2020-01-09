445221f31289eea9f89ffcf62283800e6a97ca39
Mikhail Kilin <1kilinmv1@gmail.com>
2020-01-09 22:14:43 +0300

    added validations for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  validates :period, inclusion: { in: %w(one-time daily weekly monthly annual),
+                                message: "'%{value} ' is not a period`" }
+  validates :title, length: {minimum: 3, maximum: 20}
+  validates :content, length: {minimum: 10}
+  validates_presence_of :title, :content
 end
