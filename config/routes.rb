ee281797e127ae4bba6debb11fe024e88e828f68
Mikhail Kilin <1kilinmv1@gmail.com>
2020-01-10 17:22:55 +0300

    added calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :events do
+    collection do
+      get 'by_date/:day/:month/:year', action: :by_date,
+          constraints: { day: /[0-9]+/, month: /[0-9]+/, year: /[0-9]+/ }, as: 'by_date'
+      get 'by_month/:month/:year', action: :by_month,
+          constraints: { month: /[0-9]+/, year: /[0-9]+/ }, as: 'by_month'
+    end
+  end
 end
