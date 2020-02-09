77764703000e8502b7455153d39e28b50d0b1313
Mikhail Kilin <1kilinmv1@gmail.com>
2020-02-09 17:08:36 +0300

    fixed annual and monthly scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  belongs_to :user
+
   validates :period, inclusion: { in: %w(one-time daily weekly monthly annual),
                                 message: "'%{value} ' is not a period`" }
   validates :title, length: {minimum: 3, maximum: 20}
@@ -7,9 +9,21 @@ class Event < ApplicationRecord
   scope :search_by_date, ->(date) { where("time_of_event <= ? AND time_of_event >= ? AND period = 'one-time'", date.end_of_day, date.beginning_of_day) }
   scope :search_daily, -> { where(period: :daily) }
   scope :search_weekly, ->(date) { where("extract(dow from time_of_event) = ? AND period = 'weekly'", date.wday) }
-  scope :search_monthly, ->(date) { where("extract(DAY from time_of_event) = ? AND period = 'monthly'", date.day) }
-  scope :search_annual, ->(date) { where("extract(MONTH from time_of_event) = ? AND extract(DAY from time_of_event) = ? AND period = 'annual'", date.month, date.day) }
   scope :search, ->(date) { search_by_date(date).or search_daily.or search_weekly(date).or search_monthly(date).or search_annual(date) }
 
-  belongs_to :user
+  def self.search_monthly(date)
+    if (date.at_end_of_month.day == date.day)
+      where("extract(DAY from time_of_event) >= ? AND period = 'monthly'", date.day)
+    else
+      where("extract(DAY from time_of_event) = ? AND period = 'monthly'", date.day)
+    end
+  end
+
+  def self.search_annual(date) 
+    if (date.at_end_of_month.day == date.day)
+      where("extract(MONTH from time_of_event) = ? AND extract(DAY from time_of_event) >= ? AND period = 'annual'", date.month, date.day)
+    else
+      where("extract(MONTH from time_of_event) = ? AND extract(DAY from time_of_event) = ? AND period = 'annual'", date.month, date.day)
+    end
+  end
 end
